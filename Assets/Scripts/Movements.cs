a6a9019016d520e39121edca3039b0699a414a3f
AntoninJuquel <57025128+AntoninJuquel@users.noreply.github.com>
2020-10-06 22:52:09 +0200

    Created movements script

---
+++ Assets/Scripts/Movements.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movements : MonoBehaviour
+{
+    //Movements variables
+    [SerializeField]
+    float horizontalAcceleration = 1;
+    [SerializeField]
+    [Range(0, 1)]
+    float horizontalDampingBasic = 0.5f;
+    [SerializeField]
+    [Range(0, 1)]
+    float horizontalDampingWhenStopping = 0.5f;
+    [SerializeField]
+    [Range(0, 1)]
+    float horizontalDampingWhenTurning = 0.5f;
+    //Jump variables
+    [SerializeField] float jumpVelocity = 7f;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+    }
+
+    public void Move(float horizontalInputValue)
+    {
+        float horizontalVelocity = rb.velocity.x;
+        horizontalVelocity += horizontalInputValue * horizontalAcceleration;
+
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) < 0.01f)
+            horizontalVelocity *= Mathf.Pow(1f - horizontalDampingWhenStopping, Time.deltaTime * 10f);
+        else if (Mathf.Sign(horizontalInputValue) != Mathf.Sign(horizontalVelocity))
+            horizontalVelocity *= Mathf.Pow(1f - horizontalDampingWhenTurning, Time.deltaTime * 10f);
+        else
+            horizontalVelocity *= Mathf.Pow(1f - horizontalDampingBasic, Time.deltaTime * 10f);
+
+        rb.velocity = new Vector2(horizontalVelocity, rb.velocity.y);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float jumpPressedRemember = 0, jumpPressedRememberTime = .2f, groundedRemember = 0f, groundedRememberTime = .2f;
+    [SerializeField] LayerMask whatIsGround = new LayerMask();
+
+    Movements movements;
+    BoxCollider2D col;
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        movements = GetComponent<Movements>();
+        col = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        CheckGround();
+        CheckJump();
+        HandleSpritekOrientation();
+    }
+    private void FixedUpdate()
+    {
+        movements.Move(Input.GetAxisRaw("Horizontal"));
+    }
+    void CheckGround()
+    {
+        Vector2 groundedBoxCheckPosition = (Vector2)transform.position + new Vector2(0, -0.01f);
+        Vector2 groundedBoxCheckScale = col.size + new Vector2(-0.02f, 0);
+
+        groundedRemember -= (groundedRemember>0 ? Time.deltaTime : 0);
+
+        if (Physics2D.OverlapBox(groundedBoxCheckPosition, groundedBoxCheckScale, 0, whatIsGround))
+        {
+            groundedRemember = groundedRememberTime;
+        }
+    }
+
+    void CheckJump()
+    {
+        jumpPressedRemember -= (jumpPressedRemember>0 ? Time.deltaTime : 0);
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpPressedRemember = jumpPressedRememberTime;
+        }
+
+        if (jumpPressedRemember > 0 && groundedRemember > 0)
+        {
+            jumpPressedRemember = 0;
+            movements.Jump();
+        }
+    }
+
+    void HandleSpritekOrientation()
+    {
+        if (Input.GetAxisRaw("Horizontal") < 0f)
+            spriteRenderer.transform.rotation = Quaternion.Euler(0,180,0);
+            //spriteRenderer.flipX = true;
+        else if(Input.GetAxisRaw("Horizontal") > 0f)
+            spriteRenderer.transform.rotation = Quaternion.Euler(0, 0, 0);
+            //spriteRenderer.flipX = false;
+    }
+}
